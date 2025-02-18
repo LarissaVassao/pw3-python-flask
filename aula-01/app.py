aeed6d6f560310f1c87a0a97e261f5f671a19411
LarissaVassao <larissasantosvassao@gmail.com>
2025-02-18 11:44:30 -0300

    18/02/2025 - Larissa

--- aula-01/app.py
+++ aula-01/app.py
@@ -1,15 +1,27 @@
 # Importando o Flask
-from flask import Flask
+from flask import Flask, render_template
 
 # Carregando o Flask na variável app
-app = Flask(__name__) 
+app = Flask(__name__, template_folder='views') 
+
 
 # Criando a primeira rota do site
 @app.route('/')
 # Criando função no python
 def home():
-    return '<h1>Bem vindo ao meu primeiro site em Flask!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/games')
+def games():
+    titulo = 'Stardew Valley'
+    ano = 2016
+    categoria = 'Simulação'
+    npcs =['Abigail', 'Seah', 'Sebastian', 'Harley', 'Alex,', 'Harvey', 'Emily', 'Maru', 'Eliot', 'Shane']
+    jogos = ['Rinworld', 'Roblox', 'Minecraft', 'Terraria', 'TickTowers', 'Infinity Nikki']
+    return render_template('games.html', titulo=titulo, ano=ano, categoria=categoria, npcs=npcs, jogos=jogos)
 
 # Iniciando o servidor no localhost, porta 5000, modo de depuração ativado.
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
+    
---
+++ hello-world.py
@@ -0,0 +1,1 @@
+print("Hello world! Iniciando estudos com Python!") 
