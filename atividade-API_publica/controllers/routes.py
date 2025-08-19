917b5aa72e622a826177cef092747770a27c0731
LarissaVassao <124535239+LarissaVassao@users.noreply.github.com>
2025-08-19 11:19:50 -0300

    larissa

--- atividade-API_publica/controllers/routes.py
+++ atividade-API_publica/controllers/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, url_for, redirect
 from models.database import db, Game, Console
-import urllib # Permite ler a url de uma API
-import json # Faz a conversão de dados para JSON em um dicionário de dados
+import urllib
+import json 
 
 def init_app(app):
     @app.route('/')
@@ -10,26 +10,31 @@ def init_app(app):
 
  
 
-    # Rota de catálogo de jogos (Consumo da API)
     @app.route("/apidnd", methods=['GET', 'POST'])
-    @app.route('/apidnd/<int:id>', methods=['GET', 'POST'])
+    @app.route('/apidnd/<id>', methods=['GET', 'POST'])
     def apidnd(id=None):       
-        BASE_URL = "https://www.dnd5eapi.co/api/2014"
-        response = urllib.request.urlopen(BASE_URL + "/races") 
-        apiData = response.read() # lê a resposta da API
-        raceList = json.loads(apiData) # converte JSON para dicionário
+        BASE_URL = "https://www.dnd5eapi.co"
+        response = urllib.request.urlopen(BASE_URL + "/api/2014/races") 
+        apiData = response.read() 
+        data = json.loads(apiData)
+        raceList = data["results"] 
         
         if id:
             raceInfo = []
             for race in raceList:
-               if race['id'] == id:
-                   raceInfo = race
-                   break
+               if race['index'] == id:
+                    response = urllib.request.urlopen(BASE_URL + race["url"])
+                    apiData = response.read() 
+                    raceInfo = json.loads(apiData)
+                    print(raceInfo)
+                    break
                 
-        #     if raceInfo:
-        #         return render_template("raceInfo.html", raceInfo=raceInfo)
-        #     else:
-        #         return f'Não foi possível encontrar uma raça com esse ID: {id}!'
+            if raceInfo:
+                
+                return render_template("raceInfo.html", raceInfo=raceInfo)
+            else:
+               return f'Não foi possível encontrar uma raça com esse ID: {id}!'
+             
                 
         return render_template("apidnd.html", raceList=raceList)
         
